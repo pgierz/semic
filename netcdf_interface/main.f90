c6b347409c754dec8a18453b6f700ec1a399b57d
Paul Gierz <pgierz@awi.de>
2017-03-21 09:09:09 +0100

    prints entire loop

--- netcdf_interface/main.f90
+++ netcdf_interface/main.f90
@@ -8,16 +8,18 @@ program main
   call init(smb1,"GRIS")
 
   nloop = 1
-  nt = 335
+  nt = 365
 
   do nl=1,nloop
     do t=1,nt
       call update(smb1,t)
+      ! print *,"PG: Test"
+      print *,smb1%surface%now%smb
     end do
   end do
 
-  ! print *,"PG: Test"
-  print *,smb1%surface%now%smb
+  ! ! print *,"PG: Test"
+  ! print *,smb1%surface%now%smb
 
   call surface_dealloc(smb1%surface%now)
 
